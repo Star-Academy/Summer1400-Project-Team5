96f23ca86412cb46b648771aefad5d1855e81940
Adibov <hesamadibi80@gmail.com>
2021-08-29 16:30:40 +0430

    Add new branch

--- Backend/Talent/Startup.cs
+++ Backend/Talent/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Talent.Data;
 using Talent.Data.Entities;
 using Talent.Services.Interfaces;
