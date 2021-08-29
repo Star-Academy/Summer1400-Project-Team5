d0be39fb9dbdacfd05807578f10ca79da224c6bb
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-29 22:34:35 +0430

    Refactored table design into a separate file

--- Client/src/app/pages/data-list-page/data-list-page.component.ts
+++ Client/src/app/pages/data-list-page/data-list-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-list-page',
   templateUrl: './data-list-page.component.html',
-  styleUrls: ['./data-list-page.component.scss']
+  styleUrls: ['./data-list-page.component.scss', '../../styles/base/responsive_table.scss']
 })
 export class DataListPageComponent implements OnInit {
 
