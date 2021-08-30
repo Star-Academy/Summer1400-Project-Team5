e00ea7abbcbaca5572929b30582dc31fee527ac7
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-30 16:07:57 +0430

    Configured pipes list to open pipeline on click

--- Client/src/app/pages/pipes-list-page/pipes-list-page.component.ts
+++ Client/src/app/pages/pipes-list-page/pipes-list-page.component.ts
@@ -3,6 +3,7 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
 import {AddPipeDialogComponent} from "../../dialogs/pipe/add-pipe-dialog/add-pipe-dialog.component";
 import Pipe from "../../models/pipe";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-pipes-list-page',
@@ -18,12 +19,16 @@ export class PipesListPageComponent {
     new Pipe(3, "لوله‌ی جدید"),
   ]
 
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog, private router: Router) {
   }
 
   addPipeTapped() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
     this.dialog.open(AddPipeDialogComponent, dialogConfig);
+  }
+
+  async pipeTapped(pipe: Pipe) {
+    await this.router.navigateByUrl("pipeline/" + pipe.id);
   }
 }
