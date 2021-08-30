ae866c1582a8ff8a8f83ef5465c3a9a17c9b7b67
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-30 10:52:46 +0430

    Added data details dialog component

--- Client/src/app/pages/pipes-list-page/pipes-list-page.component.ts
+++ Client/src/app/pages/pipes-list-page/pipes-list-page.component.ts
@@ -1,8 +1,8 @@
 import { Component} from '@angular/core';
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
-import {AddDataDialogComponent} from "../../dialogs/add-data-dialog/add-data-dialog.component";
-import {AddPipeDialogComponent} from "../../dialogs/add-pipe-dialog/add-pipe-dialog.component";
+import {AddDataDialogComponent} from "../../dialogs/data/add-data-dialog/add-data-dialog.component";
+import {AddPipeDialogComponent} from "../../dialogs/pipe/add-pipe-dialog/add-pipe-dialog.component";
 
 @Component({
   selector: 'app-pipes-list-page',
