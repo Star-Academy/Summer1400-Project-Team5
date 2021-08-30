663c617555dbf11ad503b1b0faeddc534b5ea07d
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-30 16:01:20 +0430

    Modified pipes list to load data dynamically

--- Client/src/app/pages/pipes-list-page/pipes-list-page.component.ts
+++ Client/src/app/pages/pipes-list-page/pipes-list-page.component.ts
@@ -2,6 +2,7 @@ import { Component} from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
 import {AddPipeDialogComponent} from "../../dialogs/pipe/add-pipe-dialog/add-pipe-dialog.component";
+import Pipe from "../../models/pipe";
 
 @Component({
   selector: 'app-pipes-list-page',
@@ -10,6 +11,12 @@ import {AddPipeDialogComponent} from "../../dialogs/pipe/add-pipe-dialog/add-pip
 })
 export class PipesListPageComponent {
   faPlus = faPlus;
+
+  pipes: Pipe[] = [
+    new Pipe(1, "لوله‌ی نخست"),
+    new Pipe(2, "لوله‌ی دوم"),
+    new Pipe(3, "لوله‌ی جدید"),
+  ]
 
   constructor(private dialog: MatDialog) {
   }
