0bf44030c4385575472fa0392f96e7f25a1f9618
bolnik <amir.bolnik@gmail.com>
2021-08-30 12:31:08 +0430

    Refactor : remove extra comment

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { DataListPageComponent } from './pages/data-list-page/data-list-page.component';
 import {PipesListPageComponent} from "./pages/pipes-list-page/pipes-list-page.component";
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-// TODO: 404 Page!
+
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', redirectTo: '', pathMatch: 'full' },
