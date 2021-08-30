a0a6c29df4126d9a491dc179d0637abc9b63771b
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-30 16:33:51 +0430

    Improvements to the pipeline page

--- Client/src/app/pages/pipeline/pipeline.component.ts
+++ Client/src/app/pages/pipeline/pipeline.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { faDatabase, faTable} from '@fortawesome/free-solid-svg-icons';
+import {ActivatedRoute} from "@angular/router";
+import Pipe from "../../models/pipe";
 
 @Component({
   selector: 'app-pipeline',
@@ -8,11 +10,20 @@ import { faDatabase, faTable} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./pipeline.component.scss']
 })
 export class PipelineComponent implements OnInit {
+  faDatabase = faDatabase;
+  faTable = faTable;
+  pipe = new Pipe(1, "لوله‌ی اول");
 
-  constructor() { }
-  faDatabase=faDatabase;
-  faTable=faTable;
+  constructor(private route: ActivatedRoute) { }
+
+  async loadPipe(id: number): Promise<Pipe> {
+    return Promise.resolve(new Pipe(2, "لوله‌ی دوم"));
+  }
+
   ngOnInit(): void {
+    this.route.queryParams.subscribe(async params => {
+      this.pipe = await this.loadPipe(parseInt(params['id']));
+    });
   }
 
 }
