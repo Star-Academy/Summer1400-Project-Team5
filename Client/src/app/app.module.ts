df0c2b664814aef36837e2db34e6532f6ea61de3
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-30 08:53:52 +0430

    Added Material module to be able to use Material dialog, needed in pipes and data views to add new items

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import {LoginPageComponent} from './pages/login-page/login-page.component';
 import {DataListPageComponent} from './pages/data-list-page/data-list-page.component';
 import {PipesListPageComponent} from './pages/pipes-list-page/pipes-list-page.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
     DataListPageComponent,
     PipesListPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
