3935286352b26130e5d18e3969fc6e7f4127f274
bolnik <amir.bolnik@gmail.com>
2021-08-31 11:01:01 +0430

    Fix : add style to add-pipe-dialog

--- Client/src/app/dialogs/pipe/add-pipe-dialog/add-pipe-dialog.component.ts
+++ Client/src/app/dialogs/pipe/add-pipe-dialog/add-pipe-dialog.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-pipe-dialog',
   templateUrl: './add-pipe-dialog.component.html',
-  styleUrls: ['./add-pipe-dialog.component.scss']
+  styleUrls: ['./add-pipe-dialog.component.scss','../../../styles/base/dialogs.scss']
 })
 export class AddPipeDialogComponent implements OnInit {
 
