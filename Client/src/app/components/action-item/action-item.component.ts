e123564692e5beb6294e93463774a8b8506d7792
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-31 10:12:32 +0430

    Improved actions styling

--- Client/src/app/components/action-item/action-item.component.ts
+++ Client/src/app/components/action-item/action-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {faAngleLeft, faDatabase} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-action-item',
@@ -8,6 +9,8 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ActionItemComponent implements OnInit {
 
   @Input() shouldShowArrowAtTheEnd = false;
+  faDatabase = faDatabase;
+  faAngleLeft = faAngleLeft;
 
   constructor() { }
 
