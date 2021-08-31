7c6ba5903b600cc8234b57b8dc1c71339a5193e0
Abolfazl Bakiasay <abolfazl850@gmail.com>
2021-08-31 12:41:00 +0430

    Added a seperate module for material imports

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,31 +1,29 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './pages/home/home.component';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {LoginPageComponent} from './pages/login-page/login-page.component';
-import {DataListPageComponent} from './pages/data-list-page/data-list-page.component';
-import {PipesListPageComponent} from './pages/pipes-list-page/pipes-list-page.component';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AddDataDialogComponent} from './dialogs/data/add-data-dialog/add-data-dialog.component';
-import {MatDialogModule} from "@angular/material/dialog";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { DataListPageComponent } from './pages/data-list-page/data-list-page.component';
+import { PipesListPageComponent } from './pages/pipes-list-page/pipes-list-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddDataDialogComponent } from './dialogs/data/add-data-dialog/add-data-dialog.component';
 import { AddPipeDialogComponent } from './dialogs/pipe/add-pipe-dialog/add-pipe-dialog.component';
 import { SQLServerDataDetailsDialogComponent } from './dialogs/data/sql-data-details-dialog/s-q-l-server-data-details-dialog.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { PipelineComponent } from './pages/pipeline/pipeline.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 import { CSVDataDetailsDialogComponent } from './dialogs/data/csv-server-data-details-dialog/c-s-v-data-details-dialog.component';
 import { ActionsListComponent } from './components/actions-list/actions-list.component';
 import { ActionItemComponent } from './components/action-item/action-item.component';
 import { EditActionDialogComponent } from './dialogs/pipe/edit-action-dialog/edit-action-dialog.component';
 import { DataTableComponent } from './components/data-table/data-table.component';
-import {MatTableModule} from "@angular/material/table";
 import { ServerRequestDialogComponent } from './dialogs/server-request-dialog/server-request-dialog.component';
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -48,16 +46,7 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
     DataTableComponent,
     ServerRequestDialogComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule,
-    BrowserAnimationsModule,
-    MatDialogModule,
-    FormsModule,
-    MatTableModule,
-    MatProgressSpinnerModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, BrowserAnimationsModule, FormsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
   entryComponents: [
@@ -65,8 +54,7 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
     AddPipeDialogComponent,
     SQLServerDataDetailsDialogComponent,
     EditActionDialogComponent,
-    ServerRequestDialogComponent
-  ]
+    ServerRequestDialogComponent,
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}
