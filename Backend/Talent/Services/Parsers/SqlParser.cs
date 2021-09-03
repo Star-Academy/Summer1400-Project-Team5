c5f3a1f5ae2fa0a0a1fc7af9562de64fa34fd17e
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-09-03 15:15:19 +0430

    SqlParser completed

--- Backend/Talent/Services/Parsers/SqlParser.cs
+++ Backend/Talent/Services/Parsers/SqlParser.cs
@@ -5,16 +5,34 @@ namespace Talent.Services.Parsers
 {
     public class SqlParser : IParser
     {
-        private Table _oldTable;
+        private readonly Table _oldTable;
+        private readonly SqlMiddleware _sqlMiddleware;
 
-        public SqlParser(Table oldTable)
+        public SqlParser(Table oldTable, SqlMiddleware sqlMiddleware)
         {
             _oldTable = oldTable;
+            _sqlMiddleware = sqlMiddleware;
         }
 
         public Table ConvertToTable(Database database, string tableName)
         {
-            throw new System.NotImplementedException();
+            var table = new Table(database, tableName);
+            InitializeTable(table);
+            return table;
+        }
+
+        private void InitializeTable(Table table)
+        {
+            foreach (Column column in _oldTable.Columns)
+            {
+                AddTableColumn(table, column);
+            }
+        }
+
+        private void AddTableColumn(Table table, Column column)
+        {
+            var newColumn = _sqlMiddleware.GetColumnByInstance(table, column.Name, column.DataType.ToString());
+            table.Columns.Add(newColumn);
         }
     }
 }
