b16c10980e3b86361cb323f1140af8e5dd67ad8c
Raha Ahmadi <r4ha.ahmadi@gmail.com>
2021-09-03 15:39:28 +0430

    SqlParser corrected

--- Backend/Talent/Services/Parsers/SqlParser.cs
+++ Backend/Talent/Services/Parsers/SqlParser.cs
@@ -6,12 +6,10 @@ namespace Talent.Services.Parsers
     public class SqlParser : IParser
     {
         private readonly Table _oldTable;
-        private readonly SqlMiddleware _sqlMiddleware;
 
-        public SqlParser(Table oldTable, SqlMiddleware sqlMiddleware)
+        public SqlParser(Table oldTable)
         {
             _oldTable = oldTable;
-            _sqlMiddleware = sqlMiddleware;
         }
 
         public Table ConvertToTable(Database database, string tableName)
@@ -25,14 +23,8 @@ namespace Talent.Services.Parsers
         {
             foreach (Column column in _oldTable.Columns)
             {
-                AddTableColumn(table, column);
+                table.Columns.Add(column);
             }
-        }
-
-        private void AddTableColumn(Table table, Column column)
-        {
-            var newColumn = _sqlMiddleware.GetColumnByInstance(table, column.Name, column.DataType.ToString());
-            table.Columns.Add(newColumn);
         }
     }
 }
