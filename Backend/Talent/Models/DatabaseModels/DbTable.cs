827e8aaef56899d1034b4ef69f1c5325c01e5e13
Adibov <hesamadibi80@gmail.com>
2021-09-04 14:08:32 +0430

    Add CreateTable method

--- Backend/Talent/Models/DatabaseModels/DbTable.cs
+++ Backend/Talent/Models/DatabaseModels/DbTable.cs
@@ -1,13 +1,62 @@
+using System;
 using System.Data;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Smo;
 
 namespace Talent.Models.DatabaseModels
 {
     public class DbTable : DataTable
     {
-        public DbTable(string? tableName) : base(tableName)
+        private readonly SqlConnection _sqlConnection;
+
+        private const int VarCharSize = 50;
+
+        public DbTable(SqlConnection sqlConnection, string? tableName) : base(tableName)
         {
+            _sqlConnection = sqlConnection;
+            if (_sqlConnection.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    _sqlConnection.Open();
+                }
+                catch
+                {
+                    throw new Exception("Cannot open the sql connection.");
+                }
+            }
         }
 
+        public int ExecuteNonQuery(string queryString)
+        {
+            var command = new SqlCommand(queryString, _sqlConnection);
+            return command.ExecuteNonQuery();
+        }
 
+        public void CreateTableInDatabase()
+        {
+            var queryString = $"CREATE TABLE {TableName} {GetColumnListString()};";
+            ExecuteNonQuery(queryString);
+        }
+
+        private string GetColumnListString()
+        {
+            var resultString = "";
+            foreach (DataColumn column in Columns)
+                resultString += column.ColumnName + " " + GetTypeInSql(column.GetType()) + ",\n";
+            return $"(\n{resultString})";
+        }
+
+        private string GetTypeInSql(Type dataType)
+        {
+            if (dataType == typeof(int))
+                return "INT";
+            if (dataType == typeof(long))
+                return "BIGINT";
+            if (dataType == typeof(double))
+                return "FLOAT";
+            return $"VARCHAR({VarCharSize})";
+        }
     }
 }
