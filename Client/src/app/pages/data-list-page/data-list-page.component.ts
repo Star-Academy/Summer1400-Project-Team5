84507bb95dae56a85ce9a419a6003ec3e8ab8fa8
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-09-08 14:50:01 +0430

    Bug fixes

--- Client/src/app/pages/data-list-page/data-list-page.component.ts
+++ Client/src/app/pages/data-list-page/data-list-page.component.ts
@@ -1,18 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faPlus} from '@fortawesome/free-solid-svg-icons';
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
 import {AddDataDialogComponent} from "../../dialogs/data/add-data-dialog/add-data-dialog.component";
-import Data from "../../models/data";
-import {DataType} from '../../models/data';
+import Data, {DataType} from "../../models/data";
 import {CSVDataDetailsDialogComponent} from "../../dialogs/data/csv-server-data-details-dialog/c-s-v-data-details-dialog.component";
 import {SQLServerDataDetailsDialogComponent} from "../../dialogs/data/sql-data-details-dialog/s-q-l-server-data-details-dialog.component";
+import RequestData, {RequestMethod} from "../../models/request-data";
+import {ServerRequestDialogComponent} from "../../dialogs/server-request-dialog/server-request-dialog.component";
 
 @Component({
   selector: 'app-data-list-page',
   templateUrl: './data-list-page.component.html',
   styleUrls: ['./data-list-page.component.scss', '../../styles/base/responsive_table.scss']
 })
-export class DataListPageComponent {
+export class DataListPageComponent implements OnInit {
   faPlus = faPlus;
 
   allData: Data[] = [
@@ -27,6 +28,7 @@ export class DataListPageComponent {
   addDataTapped() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
+    dialogConfig.disableClose = false;
     dialogConfig.data = (newData: Data) => this.allData.push(newData);
     this.dialog.open(AddDataDialogComponent, dialogConfig);
   }
@@ -43,5 +45,19 @@ export class DataListPageComponent {
         this.dialog.open(SQLServerDataDetailsDialogComponent, dialogConfig);
         break;
     }
+  }
+
+  ngOnInit(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.disableClose = true;
+    dialogConfig.data = new RequestData("data/datasource-list", RequestMethod.GET, {}, (statusCode: number, result: string) => {
+      let arr = JSON.parse(result);
+      this.allData = [];
+      for (const arrElement of arr) {
+        this.allData.push(new Data(arrElement.id, arrElement.tableName, DataType.sqlServer));
+      }
+    }); // TODO: This should be changed, obviously
+    this.dialog.open(ServerRequestDialogComponent, dialogConfig);
   }
 }
