4e38e8c3b004ed21c9e903353117be71deb216a6
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-09-15 15:18:24 +0430

    Fixed bugs

--- Client/src/app/dialogs/pipe/edit-action-dialog/edit-action-dialog.component.ts
+++ Client/src/app/dialogs/pipe/edit-action-dialog/edit-action-dialog.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { faCogs } from '@fortawesome/free-solid-svg-icons';
+import action from 'src/app/models/action';
+import ActionItem, { ActionType, JoinActionConfig } from 'src/app/models/action';
 
 @Component({
   selector: 'app-edit-action-dialog',
@@ -9,9 +12,31 @@ import { faCogs } from '@fortawesome/free-solid-svg-icons';
 export class EditActionDialogComponent implements OnInit {
   faCogs = faCogs;
 
-  constructor() { }
+  selectedType = "جمع‌آوری"; 
+  // جمع‌آوری - فیلتر - الحاق
+
+  constructor(@Inject(MAT_DIALOG_DATA) public action: ActionItem) { }
 
   ngOnInit(): void {
+    console.log(this.action.type);
+    switch (this.action.type) {
+      case ActionType.join:
+        this.selectedType = "الحاق";
+        break;
+      case ActionType.aggregate:
+        this.selectedType = "جمع‌آوری";
+        break;
+      case ActionType.filter:
+        this.selectedType = "فیلتر";
+        break;
+      default:
+        this.selectedType = "الحاق";
+        break;
+    }
+  }
+
+  submitTapped(): void {
+    alert(this.selectedType);
   }
 
 }
